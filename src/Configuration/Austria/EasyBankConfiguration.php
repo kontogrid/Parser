b22f2df0502a2ee01bcc1c20936ea1f7ac9112e8
Michael Schramm <michael.schramm@gmail.com>
2014-11-05 01:04:05 +0100

    Update EasyBankConfiguration.php

--- src/Configuration/Austria/EasyBankConfiguration.php
+++ src/Configuration/Austria/EasyBankConfiguration.php
@@ -3,10 +3,17 @@
 namespace Kontogrid\Parser\Configuration\Austria;
 
 use Kontogrid\Parser\Interfaces\ConfigurationInterface;
+use Kontogrid\Parser\Interfaces\ImportLineInterface;
 
 /**
  * @author michael.schramm@gmail.com
  */
 class EasyBankConfiguration implements ConfigurationInterface{
-
+	/**
+	 * @param ImportLineInterface $import
+	 * @return LineInterface
+	 */
+	public function convert(ImportLineInterface $import){
+	  // TODO
+	}
 }
